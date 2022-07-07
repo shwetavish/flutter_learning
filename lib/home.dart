acd8ac0c076698f1a8f0a3816c4469948e311b7e
Shweta Butani <shwetavish31@gmail.com>
2022-07-08 00:30:22 +0530

    Made Splash & Login screens. Used "Navigator" to open another screen.

---
+++ lib/home.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Flutter Demo'),
+          ),
+          body: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                buildProduct(),
+                buildProduct(),
+                buildProduct(),
+                buildProduct(),
+                buildProduct(),
+                buildProduct(),
+              ],
+            ),
+          )),
+    );
+  }
+
+  buildProduct() {
+    return Column(
+      children: [
+        Card(
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Row(
+              children: [
+                const Icon(
+                  Icons.widgets,
+                  size: 50,
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Text(
+                      'Product Name',
+                      style: TextStyle(color: Colors.black87, fontSize: 18),
+                    ),
+                    Text(
+                      'Price',
+                      style: TextStyle(color: Colors.black45, fontSize: 15),
+                    ),
+                  ],
+                ),
+                Expanded(
+                    child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: const [
+                    Icon(
+                      Icons.star,
+                      color: Colors.black38,
+                    ),
+                  ],
+                ))
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  buildProductTile() {
+    return const ListTile(
+      leading: Icon(
+        Icons.account_circle,
+        size: 50,
+      ),
+      title: Text('Product 1'),
+      subtitle: Text('Description 1'),
+      trailing: Icon(Icons.star),
+    );
+  }
+}
---
+++ lib/login.dart
@@ -0,0 +1,63 @@
+import 'package:demo_layout_widget/home.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  var nameController = TextEditingController();
+  var passwordController = TextEditingController();
+  var _validateName = false;
+  var _validatePassword = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const FlutterLogo(
+              size: 100.0,
+            ),
+            const SizedBox(
+              height: 50.0,
+            ),
+            TextField(
+              decoration: InputDecoration(hintText: 'Name', errorText: _validateName ? 'Enter Name' : null),
+              controller: nameController,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            TextField(
+              decoration: InputDecoration(hintText: 'Password', errorText: _validatePassword ? 'Enter Password' : null),
+              controller: passwordController,
+              obscureText: true,
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            ElevatedButton(onPressed: () => openHomePage(), child: const Text('Login'))
+          ],
+        ),
+      ),
+    );
+  }
+
+  openHomePage() {
+    setState(() {
+      _validateName = nameController.text.isEmpty;
+      _validatePassword = passwordController.text.isEmpty;
+    });
+
+    if (!_validatePassword && !_validateName) {
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const HomePage()));
+    }
+  }
+}
