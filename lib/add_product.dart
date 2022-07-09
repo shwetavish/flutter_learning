1dd6bcb0ca8f3b37d359e731d631802c717f0fb4
Shweta Butani <shwetavish31@gmail.com>
2022-07-09 15:46:34 +0530

    Optimize the Code & added other features: - Used "Listview" instead of "SingleChildScrollView" - Shown the Product List using both "using widgets like Row, column etc" & using "ListTile" - Add items in cart by clicking on "Add to cart" icon of each product - Added FloatingActionButton and open Add Product screen by clicking on it

---
+++ lib/add_product.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddProduct extends StatelessWidget {
+  const AddProduct({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Add Product'),
+      ),
+      body: Column(),
+    );
+  }
+}
